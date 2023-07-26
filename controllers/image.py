165ebc1713e782d3c020c6fa8d10c35b376c849c
MinhNV <minhnv@bravebits.vn>
2023-07-26 15:54:11 +0700

    add requirement

--- controllers/image.py
+++ controllers/image.py
@@ -3,10 +3,9 @@ from app import app
 from services.database_config import conn, cursor
 from services.session.session import getIdByToken, setNewSession, removeSession
 from services.upload_image import uploadImage
-from services.extract_data import extract, recognizeEntity
+from services.extract_data import extract, recognizeEntity, getBankFromText, standardizedEntity, getClosestWalletName
 from datetime import datetime
 from uuid import uuid4
-import re
 
 
 @app.route('/image/upload', methods=['post'])
@@ -35,44 +34,20 @@ def extractImage():
         textInImage = extract(filename)
         ner = recognizeEntity(textInImage)
 
-        money = ner.get('MONEY')
-        date = ner.get('DATE')
-        note = '"""' + ner.get('NOTE') + '"""' if ner.get('NOTE') else ''
-
-        money = re.sub("[^\d\.]", "", money.replace(
-            'o', '0').replace('O', '0')) if money else 0
-
-        if not date:
-            date = datetime.now()
-        else:
-            yearFirstRegex = '\d{4}\/\d{2}\/\d{2}|\d{4}\-\d{2}\-\d{2}'
-            dayFirstRegex = '\d{2}\-\d{2}\-\d{4}|\d{2}\/\d{2}\/\d{4}'
-            dateString = re.search(yearFirstRegex, date)
-            if dateString:
-                date = datetime.strptime(dateString.group(), '%Y/%m/%d')
-            else:
-                dateString = re.search(dayFirstRegex, date)
-                if dateString:
-                    date = datetime.strptime(dateString.group(), '%d/%m/%Y')
-                else:
-                    date = datetime.now()
-
-        if note:
-            textsInNote = note.split()
-            note = ' '.join([str(ele)
-                            for ele in textsInNote]).replace('"""', '')
+        extractedMoney = ner.get('MONEY')
+        extractedDate = ner.get('DATE')
+        extractedNote = ner.get('NOTE')
+        extractedBank = ner.get('BANK') or getBankFromText(textInImage)
+        [money, date, note, bank] = standardizedEntity(
+            extractedMoney, extractedDate, extractedNote, extractedBank)
 
         sql = 'select id, name from wallet where user_id=%s order by created_at'
         cursor.execute(sql, (userId, ))
         wallets = cursor.fetchall()
-        walletId = ''
         if len(wallets) == 1:
             walletId = wallets[0][0]
         else:
-            for wallet in wallets:
-                if wallet[1] != 'Tiền mặt':
-                    walletId = wallet[0]
-                    break
+            walletId = getClosestWalletName(wallets, bank)
 
         sql = 'insert into draft_transaction (id, user_id, money, money_type, created_at, note, image, access_permission, wallet_id) values (%s, %s, %s, %s, %s, %s, %s, %s, %s)'
         cursor.execute(sql, (uuid4(), userId, money,
--- services/extract_data.py
+++ services/extract_data.py
@@ -2,6 +2,8 @@ import spacy
 import os
 import pytesseract
 from PIL import Image
+import re
+from datetime import datetime
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 nlp = spacy.load("D:\WorkSpace\my-money\\backend\services\\ner\\model-best\\")
@@ -22,3 +24,58 @@ def recognizeEntity(text):
     for ent in doc.ents:
         ner[ent.label_] = ent.text
     return ner
+
+
+banks = ['vietinbank']
+def getBankFromText(text):
+  for bank in banks:
+    if (bank in text.lower()):
+      return bank
+  return 'Tiền mặt'
+
+
+def standardizedEntity(money, date, note, bank):
+    note = '"""' + note + '"""' if note else ''
+
+    money = re.sub("[^\d\.]", "", money.replace(
+        'o', '0').replace('O', '0')) if money else 0
+
+    if not date:
+        date = datetime.now()
+    else:
+        yearFirstRegex = '\d{4}\/\d{2}\/\d{2}|\d{4}\-\d{2}\-\d{2}'
+        dayFirstRegex = '\d{2}\-\d{2}\-\d{4}|\d{2}\/\d{2}\/\d{4}'
+        dateString = re.search(yearFirstRegex, date)
+        if dateString:
+            date = datetime.strptime(dateString.group(), '%Y/%m/%d')
+        else:
+            dateString = re.search(dayFirstRegex, date)
+            if dateString:
+                date = datetime.strptime(dateString.group(), '%d/%m/%Y')
+            else:
+                date = datetime.now()
+
+    if note:
+        textsInNote = note.split()
+        note = ' '.join([str(ele)
+                        for ele in textsInNote]).replace('"""', '')
+
+    bank = bank.lower()
+
+    return [money, date, note, bank]
+
+
+def getClosestWalletName(wallets, bank):
+    min = 1e9
+    walletId = ''
+    bankLength = len(bank)
+    for wallet in wallets:
+        distance = 0
+        for i in range(len(wallet[1])):
+            distance += abs(ord(wallet[1][i].lower()) -
+                            ord(bank[i])) if i < bankLength - 1 else 1
+        if (distance < min):
+            min = distance
+            walletId = wallet[0]
+    return walletId
+        
